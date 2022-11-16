ee7e9941347641f062574725faebd8e8b6f2fa7c
Olga Tsyrulyk <olga_tsyrulyk@outlook.com>
2022-11-16 14:35:19 +0800

    Added owl-carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$(function(){
+$(".owl-carousel1").owlCarousel({
+  loop: true,
+  dotsContainer: ".circles",
+  navContainer: ".arrows",
+  smartSpeed: 1200,
+  responsive: {
+    0: {
+      items: 0,
+      nav: false,
+    },
+    640: {
+      items: 1,
+      nav: true,
+    },
+  },
+});
 
+$(".owl-carousel2").owlCarousel({
+  items: 1,
+  loop: true,
+  dotsContainer: ".circles2",
+  nav: false,
+  smartSpeed: 1200,
+  autoplay: true,
+  autoplayTimeout: 5000,
 });
